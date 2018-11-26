4b0c62db722aa5c9f3088fb9387f1ed3712431ec
Blue-Silence <Blue-Silence@users.noreply.github.com>
2018-11-26 12:57:22 +0800

    ~

--- Calculator/Value.hs
+++ Calculator/Value.hs
@@ -3,20 +3,18 @@ module Value(
 )where
 
 import DataType
-import Transfer
 import Tools
 
-value :: OptList_All->Exp->Exp
-value x=valuef x x
 
-valuef :: OptList_All->OptList_All->Exp->Exp
-valuef all _ (Num value)=(Num value)
-valuef all _ (Exp (x:[]))=valuef all all x
-valuef all listnow (Exp (x:opt:y:z))=if belong listb optx 
-                                     then valuef all listnow (Exp ([(Num ((getopt listb optx) valuex valuey))] ++ z))
-                                     else valuef all lista (Exp (x:opt:(valuef all listnow (Exp ([y]++z))):[]))                      
-                                            where (Num valuex)=valuef all all x
-                                                  (Num valuey)=valuef all all y
+
+value :: OptList_All->OptList_All->Exp->Exp
+value all _ (Num value)=(Num value)
+value all _ (Exp (x:[]))=value all all x
+value all listnow (Exp (x:opt:y:z))=if belong listb optx 
+                                     then value all listnow (Exp ([(Num ((getopt listb optx) valuex valuey))] ++ z))
+                                     else value all lista (Exp (x:opt:(value all listnow (Exp ([y]++z))):[]))                      
+                                            where (Num valuex)=value all all x
+                                                  (Num valuey)=value all all y
                                                   (Opt optx)=opt
                                                   listb=car listnow
                                                   lista=cdr listnow
---
+++ Calculator/Value_main.hs
@@ -0,0 +1,5 @@
+module Value_mk(value_mk)where
+import Transfer
+import Value
+
+value_mk x =value x x . transfer
